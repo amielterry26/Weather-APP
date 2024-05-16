490f060109a303b71ec4dfe9e2c18d69415ba0e2
Amiel Terry <amielterry.dev@gmail.com>
2024-05-15 22:29:14 -0700

    Initial Commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Amiel Terry on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
