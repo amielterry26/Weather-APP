953312f11662ac783aa7789e4a059e49f0937733
Amiel Terry <amielterry.dev@gmail.com>
2024-05-16 02:16:50 -0700

    first commit

---
+++ Weather-App/Managers/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  Weather-App
+//
+//  Created by Amiel Terry on 5/15/24.
+//
+
+import Foundation
---
+++ Weather-App/Views/Welcome View.swift
@@ -0,0 +1,18 @@
+//
+//  Welcome View.swift
+//  Weather-App
+//
+//  Created by Amiel Terry on 5/15/24.
+//
+
+import SwiftUI
+
+struct Welcome_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcome_View()
+}
